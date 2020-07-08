106f9e7564ad293982615b871eb85cb4d728b165
Shalitha Jayamal <47962458+ShalithaJayamal@users.noreply.github.com>
2020-07-08 23:48:45 +0530

    updated

--- Lavish-Sales-Force-Automation-Backend/server.js
+++ Lavish-Sales-Force-Automation-Backend/server.js
@@ -27,7 +27,7 @@ mongoose.connect(dbConfig.url, {
 
 const users = require('./app/routes/user.routes.js');
 
-app.use(function(req, res, next) {
+app.use(function(req, res,  next) {
     res.header("Access-Control-Allow-Origin", "*");  
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
     next();
